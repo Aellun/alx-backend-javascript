d640fdfa9eb5e725d1e6be00c3a7fc6018f6c191
aellun <okello.k.otieno@gmail.com>
2024-07-25 00:20:33 +0300

    javascript backend; promises

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,13 @@
+// exported prototype function
+export default function getFullResponseFromAPI(success) {
+    return new Promise((resolve, reject) => {
+      if (success) {
+        // Resolve the promise with an object containing status and body
+        resolve({ status: 200, body: 'Success' });
+      } else {
+        // Reject the promise with an error message
+        reject(new Error('The fake API is not working currently'));
+      }
+    });
+  }
+  
---
+++ README.md
@@ -0,0 +1,1 @@
+## Contains all backend javascript project solutions
