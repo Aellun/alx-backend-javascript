ebb330a3056d4845cfabdf6f777b2ea7d8478d63
aellun <okello.k.otieno@gmail.com>
2024-07-25 04:38:56 +0300

    lint correction

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,11 @@
 /* eslint-disable no-console */
 export default function handleResponseFromAPI(promise) {
-    return promise
+  return promise
     // Return an object with status and body
-      .then(() => ({ status: 200, body: 'success' }))
-      // Return an empty Error object when promise rejects
-      .catch(() => new Error())
-      .finally(() => {
-        console.log('Got a response from the API');
-      });
-  }
-  
+    .then(() => ({ status: 200, body: 'success' }))
+  // Return an empty Error object when promise rejects
+    .catch(() => new Error())
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
+}
