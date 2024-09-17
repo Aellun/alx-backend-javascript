c36cd5a3603c222ebd1abc6404ba7bc5e5e77fe4
Aellun <okello.k.otieno@gmail.com>
2024-09-17 22:28:55 +0300

    0. Executing basic javascript with Node JS

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,11 @@
+// Function displayMessage
+
+/**
+ * Prints the given message to STDOUT.
+ * @param {string} message - The message to print.
+ */
+function displayMessage(message) {
+    console.log(message);
+}
+
+module.exports = displayMessage;
