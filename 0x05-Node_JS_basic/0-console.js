88c4a3bafc0065dc28398f30cb5975b5a29c15b7
Aellun <okello.k.otieno@gmail.com>
2024-09-17 22:33:28 +0300

    updated

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,9 +1,3 @@
-// Function displayMessage
-
-/**
- * Prints the given message to STDOUT.
- * @param {string} message - The message to print.
- */
 function displayMessage(message) {
     console.log(message);
 }
