bca5c0b241f54dc16efeb1f4d6f338a0d52c1298
Aellun <okello.k.otieno@gmail.com>
2024-09-18 05:15:40 +0300

    updated

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,17 +1,18 @@
-// Displays the welcome message
+// Display the initial message
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Handles user input
-process.stdin.on('data', (data) => {
-  // Display the name of the user
-  process.stdout.write(`Your name is: ${data.toString().trim()}\n`);
-
-  // Close the stdin stream
-  process.stdin.end();
-});
-
-// Handles the end of the program
-process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
-  process.exit();
-});
+// checks if the input environment is terminal
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();// Exit immediately after processing input
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
