7524bd34acc53ab6b7908c83f99ce591493cc7ff
Aellun <okello.k.otieno@gmail.com>
2024-09-18 05:08:59 +0300

    updated the 0-console

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,9 +1,10 @@
+// displaymessage function
 /**
- * Prints the given message to STDOUT.
+ * Prints the provided message to STDOUT.
  * @param {string} message - The message to print.
  */
 function displayMessage(message) {
-    console.log(message);
+  console.log(message); // Print the message to the standard output
 }
 
 module.exports = displayMessage;
